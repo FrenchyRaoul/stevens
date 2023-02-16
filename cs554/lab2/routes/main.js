477c278a2d2952e23230f95274e10e5923f6ba57
nlespera <nlespera@us.ibm.com>
2023-02-16 10:40:43 -0500

    adding scores to mostaccessed route

--- cs554/lab2/routes/main.js
+++ cs554/lab2/routes/main.js
@@ -55,15 +55,34 @@ router.get('/logout', async (req, res) => {
     res.json({"logout": true})
 })
 
+// router.get('/mostaccessed', async (req, res) => {
+//     try {
+//         const client = await getRedis();
+//         const recipeIds = (await client.zRange(hitCountKey, 0, -1, {
+//             "REV": true,
+//             "BYSCORE": true,
+//         })).slice(0, 10);
+//         const recipes = (await client.hmGet(recipeCacheKey, recipeIds)).map(JSON.parse);
+//         res.json(recipes);
+//     } catch (e) {
+//         res.status(500).json({"error": e});
+//     }
+// })
 
 router.get('/mostaccessed', async (req, res) => {
+    const max_results = 2;
     try {
         const client = await getRedis();
-        const recipeIds = (await client.zRange(hitCountKey, 0, -1, {
-            "REV": true,
-            "BYSCORE": true,
-        })).slice(0, 10);
-        const recipes = (await client.hmGet(recipeCacheKey, recipeIds)).map(JSON.parse);
+        const results = (await client.sendCommand(["ZRANGE", hitCountKey, "0", "-1", "REV", "WITHSCORES"])).slice(0, max_results * 2);
+        let ids = [];
+        let hits = [];
+        results.forEach((value, index) => {
+            (index % 2 === 0) ? ids.push(value) : hits.push(value);
+        })
+        const recipes = (await client.hmGet(recipeCacheKey, ids)).map(JSON.parse);
+        recipes.forEach((recipe, index) => {
+            recipe['hits'] = hits[index];
+        })
         res.json(recipes);
     } catch (e) {
         res.status(500).json({"error": e});
