d8c06f5f0ba37e4c6c4223183f602fd27bbda8f8
nlespera <nlespera@us.ibm.com>
2023-04-03 18:01:28 -0400

    first lab5 commit

---
+++ cs554/lab5/src/place/Places.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import {query} from "express";
+
+const PAGE_SIZE = 25;
+
+const authConfig = {
+    headers:{
+        Authorization: process.env.REACT_APP_TM_APIKEY
+    }
+};
+
+async function getPhotosFromId(fsq_id) {
+    if (fsq_id === null) {
+        throw "cannot query photos for null fsq_id"
+    }
+    const photoUrl = `https://api.foursquare.com/v3/places/${fsq_id}/photos`
+    return await axios.get(photoUrl, authConfig)
+}
+
+async function searchPlaces(queryParams) {
+    if (queryParams === null || !queryParams.entries.length) {
+        throw "query parameters must be provided to use search api"
+    }
+    const queryString = queryParams.entries.map((key, value)=>{
+        return `${key}=${value}`
+    }).join("&");
+    const searchUrl = "https://api.foursquare.com/v3/places/search?" + queryString;
+    return await axios.get(searchUrl, authConfig)
+}
+
+
+export {
+    getPhotosFromId
+}
